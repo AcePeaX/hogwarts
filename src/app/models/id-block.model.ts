ef5747c49f0bd6e2378620cfc549ca83a627f4c3
Mohammed Benmansour <benmansour.mohammed2002@gmail.com>
2024-03-28 18:41:21 +0100

    Design complete, implemented IdBlock class

---
+++ src/app/models/id-block.model.ts
@@ -0,0 +1,11 @@
+export class IdBlock{
+  constructor(id:number,
+    public firstName: string,
+    public lasttName: string,
+    public description: string,
+    public arrivalDate: Date,
+    public house: string,
+    public assignment:string,
+    public isTeacher:boolean) {
+}
+}
